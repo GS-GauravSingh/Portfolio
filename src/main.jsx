8dbb2185a2c6c815b0bb0d86da8ec14e00efa18f
GauravSingh/WSLDesktop <gs062254@gmail.com>
2024-10-27 17:01:02 +0000

    update import statement in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Layout from "./Layout";
 import "./main.css";
-import "./css/MediaQueries.css"
+import "./css/mediaQueries.css"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
